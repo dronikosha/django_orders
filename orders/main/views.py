6b2f8dcf9251ef738c4b1806f8ed174fb5ccb1c1
dronikosha <dronikosha@gmail.com>
2022-10-13 15:48:02 +0300

    pep8

--- orders/main/views.py
+++ orders/main/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
 from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
 
 from .models import Order
-
 
 
 def owner_check(func):
